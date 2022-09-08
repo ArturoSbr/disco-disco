ba6e4f5cf6edda4e669c8f20620525c9d8c82d30
Arturo <soberonarturo@kinoo.family>
2022-09-07 23:44:53 -0500

    Function to prep data

--- disco.py
+++ disco.py
@@ -11,11 +11,51 @@ metrics = {
     'mae': mean_absolute_error
 }
 
+# Function to prepare data
+def prep_data(
+    data: pd.DataFrame = None,
+    dependent_variable: str = None,
+    running_variable: str = None,
+    cutoff: float = 0,
+    treated: str = 'right',
+    degree: int = 1
+):
+
+    # Reindex data
+    ret = data.reset_index(drop=True)
+
+    # Declare constant column
+    ret['const'] = 1
+
+    # Recenter running variable at zero
+    ret[running_variable] = ret[running_variable] - cutoff
+
+    # Declare treatment column
+    if treated == 'right':
+        ret['treat'] = ret[running_variable].ge(0).astype(int)
+    else:
+        ret['treat'] = ret[running_variable].le(0)
+
+    # Declare powers and interactions
+    for d in range(degree):
+        ret[f'{running_variable}_pow{d+1}'] = ret[running_variable].pow(d+1)
+        ret[f'{running_variable}_treat_pow{d+1}'] = (ret[running_variable] * ret['treat']).pow(d+1)
+    
+    # Columns with _pow in their name
+    cols = [dependent_variable, 'const', 'treat'] + [col for col in ret.columns if '_pow' in col]
+
+    # Organize prepped data
+    ret = ret[cols]
+
+    # Return DataFrame
+    return ret
+
 # Function to return CV scores
 def cv_bandwidths(
     data: pd.DataFrame = None,
-    running_variable: str = None,
     dependent_variable: str = None,
+    running_variable: str = None,
+    degree: int = 1,
     n_bandwidths: int = 10,
     folds: int = 5,
     criteria: str = 'mse',
